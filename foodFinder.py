9747080d3e608970a0fc10910f1b92c9fbf6c108
Elaine <secondezheng@gmail.com>
2021-03-06 15:55:02 -0500

    Styling, and form, back and forth buttons

--- foodFinder.py
+++ foodFinder.py
@@ -9,5 +9,10 @@ def index():
     return render_template('index.html')
 
 
+@app.route('/donate_food')
+def donate_food():
+    return render_template('donate_food.html')
+
+
 if __name__ == '__main__':
     app.run()

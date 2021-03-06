4b9d2b4ecd49f671a919f0712dbd4780f1817d3f
Dean-Sussman <sussmandean@icloud.com>
2021-03-06 18:19:13 -0500

    flask stuff

--- foodFinder.py
+++ foodFinder.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+import mechanize
+from flask import Flask, render_template, request, redirect
+
+from main import writeFoodBanks
 
 app = Flask(__name__)
 
@@ -7,6 +10,22 @@ app = Flask(__name__)
 def index():
     # Displays the index page accessible at '/'
     return render_template('index.html')
+
+
+@app.route('/address', methods=['GET', 'POST'])
+def donate():
+    if request.method == "POST":
+        street_address = request.form['address']
+        city = request.form['city']
+        state = request.form['states']
+        pass_to_backend(street_address, city, state)
+        return redirect('/')
+    else:
+        return render_template("index.html")
+
+
+def pass_to_backend(street_address, city, state):
+    writeFoodBanks(mechanize.Browser(), street_address, city, state);
 
 
 @app.route('/donate_food')
--- main.py
+++ main.py
@@ -8,15 +8,17 @@ class foodPantry:
         self.foodItems = str(foodItems)
 
 
-def writeFoodBanks(br):
+def writeFoodBanks(br, street_address, city, state):
     br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) '
                                     'Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
     br.open("https://www.foodpantries.org/st/new_jersey")
+    print(br.title())
     br.form = list(br.forms())[0]
-    br["address"] = "1 Fake Street"
-    br["city"] = "New Brunswick"
+    br["address"] = street_address
+    br["city"] = city
     controlState = br.form.find_control("state")
-    br[controlState.name] = ["nj"]
+    br[controlState.name] = [state]
+    # br["states"] = state
     response = br.submit()
     with open("foodBanks.txt", "wb") as code:
         code.write(response.read())
@@ -69,7 +71,7 @@ def getFoodItems(browserData):
 def main():
     br = mechanize.Browser()
     br.set_handle_robots(False)  # ignore robots
-    writeFoodBanks(br)
+   # writeFoodBanks(br)
     foodBankUrls = getFoodBankUrls(br)
     foodPantries = []
     for url in foodBankUrls:

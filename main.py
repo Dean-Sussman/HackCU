bad04dfcace373b292ed5a70fe39e04b4be83276
Diane <dianetran24141@gmail.com>
2021-03-06 15:25:25 -0500

    read urls from nearby foodbanks

--- main.py
+++ main.py
@@ -1,23 +1,43 @@
 import mechanize
-br = mechanize.Browser()
-br.set_handle_robots(False)   # ignore robots
 
-br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-br.open("https://www.foodpantries.org/st/new_jersey")
-# print(response.read())      # the text of the page
-print(br.title())
-br.form = list(br.forms())[0]
-br["address"] = "1 Fake Street"
-br["city"] = "New Brunswick"
-controlState = br.form.find_control("state")
-br[controlState.name] = ["nj"]
-response = br.submit()
-# print(response.read())
-with open("temp.txt", "wb") as code:
-    code.write(response.read())
 
-    #TO-DO: Extract food pantry websites (3 max.)
-    #Navigate to those websites
-    #Get food items
-        # Or statement for what to look for when web-scraping (Items in need, donate food, etc.)
-        # Take items in list and add them to our website!
+def writeFoodBanks(br):
+    br.addheaders = [('User-agent',
+                      'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+    br.open("https://www.foodpantries.org/st/new_jersey")
+    print(br.title())
+    br.form = list(br.forms())[0]
+    br["address"] = "1 Fake Street"
+    br["city"] = "New Brunswick"
+    controlState = br.form.find_control("state")
+    br[controlState.name] = ["nj"]
+    response = br.submit()
+    with open("foodBanks.txt", "wb") as code:
+        code.write(response.read())
+
+
+def writeFoodItems(br, url, i):
+    br.open("url")
+    print(br.title())
+    response = br.response()
+    with open("foodItems" + i + ".txt", "wb") as code:
+        code.write(response.read())
+
+
+def getFoodBanks():
+    file = open("foodBanks.txt", 'r').read().split(" ")
+    prefix = "href=\"https://www.foodpantries.org/li/"
+    suffix = ">Website</a></span>"
+    for line in file:
+        if line.startswith(prefix) and line.endswith(suffix):
+            print(line[len(prefix):-len(suffix)])
+
+
+def main():
+    br = mechanize.Browser()
+    br.set_handle_robots(False)  # ignore robots
+    writeFoodBanks(br)
+    getFoodBanks()
+
+    # writeFoodItems(br, "https://www.foodpantries.org/li/elijahs_promise_08901", '0')
+main()

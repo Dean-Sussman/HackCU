0f83bf209ea45eb392de9155c98985c0eec053e4
Elaine <secondezheng@gmail.com>
2021-03-07 13:42:24 -0500

    Eggs

--- static/scripts.js
+++ static/scripts.js
@@ -6,6 +6,12 @@ var impactNum = document.getElementById("impact");
 var egg1 = document.getElementById("eggs1");
 var egg2 = document.getElementById("eggs2");
 var egg3 = document.getElementById("eggs3");
+var egg4 = document.getElementById("eggs4");
+var egg5 = document.getElementById("eggs5");
+var egg6 = document.getElementById("eggs6");
+var egg7 = document.getElementById("eggs7");
+var egg8 = document.getElementById("eggs8")
+
 var clicks = 0;
 
 function toggleDoor() {
@@ -13,19 +19,33 @@ function toggleDoor() {
   element.classList.toggle("doorOpen");
   if (clicks %2 == 1) { //when door is opened
     tt.style.display = "none";
-    if (impactEggs > 5) {
+    if (impactEggs > 7) {
      egg1.style.display = "block";
      egg2.style.display = "block";
      egg3.style.display = "block";
+     egg4.style.display = "block";
+     egg5.style.display = "block";
+     egg6.style.display = "block";
+     egg7.style.display = "block";
+     egg8.style.display = "block";
     } else if(impactEggs > 2) {
      egg1.style.display = "block";
      egg2.style.display = "block";
-    } else {
+     egg3.style.display = "block";
+    } else if(impactEggs > 0) {
       egg1.style.display = "block";
     }
   } else {
   setTimeout(function(){
     tt.style.display = "block";
+    egg1.style.display = "none";
+     egg2.style.display = "none";
+     egg3.style.display = "none";
+     egg4.style.display = "none";
+     egg5.style.display = "none";
+     egg6.style.display = "none";
+     egg7.style.display = "none";
+     egg8.style.display = "none";
 }, 500);
 
   }
